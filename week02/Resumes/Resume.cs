815d0f5a20c5721abf98b17d40cb88cd1509021e
Ugofebe <ugofebe@gmail.com>
2025-05-14 11:34:06 +0100

    week2 resume task

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+
+    // Initializing my list to a new list 
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
